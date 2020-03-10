5aab0d186fee6ba3086595491a0978618b30a6cd
Ethan Fournier <ethanjosephfournier@gmail.com>
2020-03-10 17:48:01 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
+  
+  attr_accessor :title, :author_name, :page_count, :genre
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  def turn_page
+    puts "turned the page"
+  end
+end
 
 
